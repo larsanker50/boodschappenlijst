9c7e560b39a8fa580052b6abcefc023f0f15d41c
Lars Anker <75078152+larsanker50@users.noreply.github.com>
2021-04-06 11:26:46 +0200

    2020

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const inputBrood = document.getElementsByName('brood')[0];
-const inputBrocolli = document.getElementsByName('brocolli')[0];
-const inputKrentenbollen = document.getElementsByName('krentenbollen')[0];
-const inputNoten = document.getElementsByName('noten')[0];
+const inputBrood = document.getElementById('inputBrood');
+const inputBrocolli = document.getElementById('inputBrocolli');
+const inputKrentenbollen = document.getElementById('inputKrentenbollen');
+const inputNoten = document.getElementById('inputNoten');
 
-const subTotaalBrood = document.getElementsByClassName('productTotalCostA')[0];
-const subTotaalBrocolli = document.getElementsByClassName('productTotalCostB')[0];
-const subTotaalKrentenbollen = document.getElementsByClassName('productTotalCostC')[0];
-const subTotaalNoten = document.getElementsByClassName('productTotalCostD')[0];
-const subTotaal = document.getElementsByClassName('totalCost')[0];
+const subTotaalBrood = document.getElementById('productTotalCostA');
+const subTotaalBrocolli = document.getElementById('productTotalCostB');
+const subTotaalKrentenbollen = document.getElementById('productTotalCostC');
+const subTotaalNoten = document.getElementById('productTotalCostD');
+const subTotaal = document.getElementById('totalCost');
 
 inputBrood.addEventListener('change',() => {
     updateValueBrood();
@@ -32,30 +32,30 @@ inputNoten.addEventListener('change',() => {
 
 function updateValueBrood() {
     math = (1 * +inputBrood.value);
-    subTotaalBrood.innerHTML = math;
-    return math;
+    subTotaalBrood.innerHTML = math.toFixed(2);
+    return math.toFixed(2);
 }
 
 function updateValueBrocolli() {
     math = (0.99 * +inputBrocolli.value);
-    subTotaalBrocolli.innerHTML = math;
-    return math;
+    subTotaalBrocolli.innerHTML = math.toFixed(2);
+    return math.toFixed(2);
 }
 
 function updateValueKrentenbollen() {
     math = (1.20 * +inputKrentenbollen.value);
-    subTotaalKrentenbollen.innerHTML = math;
-    return math;
+    subTotaalKrentenbollen.innerHTML = math.toFixed(2);
+    return math.toFixed(2);
 }
 
 function updateValueNoten() {
     math = (2.99 * +inputNoten.value);
-    subTotaalNoten.innerHTML = math;
-    return math;
+    subTotaalNoten.innerHTML = math.toFixed(2);
+    return math.toFixed(2);
 }
 
 function updateTotalCost() {
     math = (+updateValueBrood() + +updateValueBrocolli() + +updateValueKrentenbollen() + +updateValueNoten());
-    subTotaal.innerHTML = math;
-    return math;
+    subTotaal.innerHTML = math.toFixed(2);
+    return math.toFixed(2);
 }
